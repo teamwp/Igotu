cff06c5f120f4d582c4be4701e1ba8761f9ca731
Harmon Huynh <harmon.huynh@outlook.com>
2019-01-17 20:11:32 -0800

    Completed file structure

---
+++ client/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import MainContainer from '/containers/MainContainer';
+
+class Moo extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        This is Moo
+      </div>
+    )
+  }
+}
+
+export default Moo;
---
+++ client/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Moo from './App.jsx';
+
+ReactDom.render(<Moo></Moo>, document.getElementById('moo'));
