691f2623c0ba611643cdd7af1ae759b395c2bfd9
Byron Inocencio <jay.byron@gmail.com>
2019-01-18 19:22:39 -0800

    implemented HTTP GET route for querying all items

--- server/controllers/items-controller.js
+++ server/controllers/items-controller.js
@@ -1,7 +1,10 @@
 const pg = require('pg');  // postgres library
 
+
+itemsController = {};
+
 // change to get/show all items
-userController.getUsers((req, res, next) => {
+itemsController.getUsers = ((req, res, next) => {
   // code here
   const uri = 'postgresql://igotu:eyegotchu@igotu-master.cu4n5g8jahnw.us-west-2.rds.amazonaws.com:5432/igotu';
   const pool = new pg.Pool({
@@ -19,3 +22,28 @@ userController.getUsers((req, res, next) => {
   });
   next();
 });
+
+itemsController.getAllItems = ( (req,res, next) => {
+
+  const uri = 'postgresql://igotu:eyegotchu@igotu-master.cu4n5g8jahnw.us-west-2.rds.amazonaws.com:5432/igotu';
+  
+  const pool = new pg.Pool({
+    connectionString: uri,
+  });
+
+  const query = {
+    text: 'SELECT * FROM items'
+  }
+
+  pool.query(query.text, (err, items) => {
+    if (err) {
+      console.log('Here\'s the error: ' + err);
+    } else {
+      console.log(items.rows);
+      res.locals.items = items.rows;
+      next();
+    };
+  });
+});
+
+module.exports = itemsController;
