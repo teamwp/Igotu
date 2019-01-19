860aa3cdf1bd36f5cf19e4cacfa737bf68053160
emiliacarmel <emiliacarmel@gmail.com>
2019-01-18 20:34:56 -0800

    completed http route for adding an item, began auth but put on hold

--- server/controllers/sessions-controller.js
+++ server/controllers/sessions-controller.js
@@ -1,3 +1,19 @@
-const pg = require('pg');  // postgres library
-const sessionController = require('./users-controller');
 
+// COME BACK TO THIS!!!
+// DELAYING SESSION SIGN IN UNTIL POST ITEMS AND QUERY DB ARE COMPLETED
+
+
+const pg = require('pg');  // postgres library
+const sessionsController = require('./users-controller');
+
+
+const sessionController = {};
+
+sessionController.setCookie = (req, res, next) => {
+  res.cookie('codesmith', 'hi');
+  res.cookie('secret', Math.floor(Math.random() * 99));
+  console.log('Here\'s the res.cookie: ' + res.cookie);
+  next();
+};
+
+module.exports = sessionController;
--- server/server.js
+++ server/server.js
@@ -5,25 +5,52 @@ const app = express();
 const port = 3000;
 require('dotenv').config()
 const userController = require('./controllers/users-controller');
+const itemController = require('./controllers/items-controller');
+// const sessionController = require('./controllers/sessions-controller');
 const pg = require('pg');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cookieParser());
 
-
-// home page
 app.get('/', (req, res, err) => {
   // home page
 });
 
+app.get('/allItems', (req, res, err) => {
+  // displays all items from items table
+});
 
-// add a user/sign up
+app.get('/user/:email', (req, res, err) => {
+  // joins user table and item table
+});
+
+app.get('/item/:id', (req, res, err) => {
+  // gets item from item table
+});
+
+app.get('/search/:item', (req, res, err) => {
+  // queries item table for item
+});
+
+app.get('/category/:category', (req, res, err) => {
+  // queries items by category
+});
+
 app.post('/addUser', userController.addUser, (req, res, err) => {
-  console.log(res.locals.data)
   res.status(200).json(res.locals.data);
 });
+
+app.post('/addItem', itemController.addItem, (req, res, err) => {
+  res.status(200).json(res.locals.data);
+})
+
+app.delete('/deleteItem', (req, res, err) => {
+  // deletes item from database
+})
 
 
 app.listen(port, () => console.log('Listening on port ' + port));
