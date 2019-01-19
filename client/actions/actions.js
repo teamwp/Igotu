c7102c96202b187804448be486cac5807ecf3e0a
Harmon Huynh <harmon.huynh@outlook.com>
2019-01-18 16:20:41 -0800

    Added fetch package. Wrote first action creator to fetch all items from database.

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,22 +1,34 @@
+import fetch from 'whatwg-fetch';
 import * as types from '../constants/actionTypes';
 
-export const getAllItemsStart = () => ({
+export const fetchItemsStart = () => ({
   type: types.GET_ALL_ITEMS_START,
   fetching: true
 });
 
-export const getAllItems = resp => ({
+export const fetchedItems = resp => ({
   type: types.GET_ALL_ITEMS,
   fetching: false,
   fetched: true,
   payload: resp
 });
 
-export const getAllItemsErr = err => ({
+export const fetchError = err => ({
   type: types.GET_ALL_ITEMS_ERR,
   fetching: false,
   payload: err
 });
+
+export const fetchItemsData = () => dispatch => {
+  dispatch(fetchItemsStart);
+
+  fetch('/allItems')
+    .then(response => {
+      console.log(response);
+      dispatch(fetchedItems(response.json()));
+    })
+    .catch(() => dispatch(fetchError));
+};
 
 // export const searchStart = query => ({
 //   type: types.SEARCH,
